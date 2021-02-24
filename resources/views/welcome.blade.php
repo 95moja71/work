aff90f1dad7aaa1d68f6ceadeeb1d9ff2d1ece43
95moja71 <mohammad.abas71@gmail.com>
2021-02-24 16:21:16 +0330

    commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -117,7 +117,7 @@
                             </svg>
 
                             <a href="https://github.com/sponsors/taylorotwell" class="ml-1 underline">
-                                Sponsor
+                               kk
                             </a>
                         </div>
                     </div>
